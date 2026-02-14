697b8603a93bbbfecf2fc28c86e1061878fd57a1
NishiSingh0408 <nishi.singh_cs25@gla.ac.in>
2026-02-14 19:36:55 +0530

    Add files via upload

---
+++ CountPrime.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+    public class CountPrime{
+          public static void main(String[] args){
+             Scanner sc = new Scanner(System.in);
+             int n = sc.nextInt();
+             if(n<=2){
+               System.out.println(0);
+                return;
+             }
+             boolean[] isPrime = new boolean[n];
+             for(int i = 2; i < n; i++){
+                 isPrime[i] = true;
+             }
+             for(int i =2; i*i<n; i++){
+                if(isPrime[i]){
+                   for(int j = i*i; j<n; j+=i){
+                       isPrime[j] = false;
+                   }
+                }
+             }
+             int count = 0;
+             for(int i=2; i<n; i++){
+                 if(isPrime[i]){
+                    count++;
+                 }
+             }
+             System.out.println(count);
+ }
+}
