ac09f4b1c4c16a3986ac41281d4d0e74744939cf
NishiSingh0408 <nishi.singh_cs25@gla.ac.in>
2026-02-25 14:05:11 +0530

    Add files via upload

---
+++ SumOfDigitsInBaseK.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+      public class SumOfDigitsInBaseK{
+           public static void main(String[] args){
+              Scanner sc = new Scanner(System.in);
+                int n = sc.nextInt();
+                int k = sc.nextInt();
+                int sum = 0;
+                while(n>0){
+                  sum+= n%k;
+                    n/=k;
+                } 
+                 System.out.println(sum);
+ }
+}
