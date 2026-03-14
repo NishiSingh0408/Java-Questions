6e5fe73d83b8b03f1b576e99531821ae4e0be1bc
NishiSingh0408 <nishi.singh_cs25@gla.ac.in>
2026-03-14 16:16:55 +0530

    Add files via upload

---
+++ LengthOfString.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+   public class LengthOfString{
+       public static void main(String[] args){
+         Scanner sc = new Scanner(System.in);
+         String str = sc.nextLine();
+         int count = 0;
+         for(char c : str.toCharArray()){
+           count++;
+         }
+         System.out.println(count);
+ }
+}
