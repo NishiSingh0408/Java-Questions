02f8161661ca9bebe2a128b8cc3e4a7719a7be92
NishiSingh0408 <nishi.singh_cs25@gla.ac.in>
2026-03-17 19:44:31 +0530

    Add files via upload

---
+++ ConvertToUpperCase.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+    public class ConvertToUpperCase{
+        public static void main(String[] args){
+          Scanner sc = new Scanner(System.in);
+          String str = sc.nextLine();
+          char[] ch = str.toCharArray();
+          for(int i=0; i<ch.length; i++){
+              if(ch[i] >= 'a' && ch[i] <= 'z'){
+                  ch[i] = (char)(ch[i] -32);
+              }
+          }
+          System.out.println(new String(ch));
+ }
+}
