b388af14d22034250373d833ba1b0b4757ec9e67
NishiSingh0408 <nishi.singh_cs25@gla.ac.in>
2026-03-30 18:18:56 +0530

    Add files via upload

--- MajorityElement.java
+++ MajorityElement.java
@@ -1,16 +1,25 @@
-import java.util.Arrays;
 import java.util.Scanner;
    public class MajorityElement{
-      public static void main(String[] args){
+       public static void main(String[] args){
          Scanner sc = new Scanner(System.in);
-         int n = sc.nextInt();
-         int[] nums = new int[n];
-         for(int i=0; i<n; i++){
-           nums[i] = sc.nextInt();
-         }
-         if(n>0){
-         Arrays.sort(nums);
-         System.out.println(nums[nums.length/2]);
-         }
+          int n = sc.nextInt();
+          int[] arr = new int[n];
+          for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+          }
+          int count = 0;
+          int candidate = 0;
+          for(int num:arr){
+            if(count==0){
+              candidate = num;
+            }
+            if(num==candidate){
+              count++;
+            }
+            else{
+              count--;
+            }
+          }
+          System.out.println(candidate);
  }
 }
