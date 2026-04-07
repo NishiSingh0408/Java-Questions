ade7241307e0d0a23448512daa8dea5b5a914b64
NishiSingh0408 <nishi.singh_cs25@gla.ac.in>
2026-04-07 20:12:38 +0530

    Add files via upload

---
+++ PairWithGivenDifference.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+  public class PairWithGivenDifference{
+      public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+         for(int i=0; i<n; i++){
+           arr[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        boolean found = false;
+        for(int i=0; i<n; i++){
+           for(int j=i+1; j<n; j++){
+              if(Math.abs(arr[i] - arr[j]) == k){
+                System.out.println(i + " " + j);
+                found = true;
+                break;
+              }
+           }
+            if(found) break;
+        }
+        if(!found){
+           System.out.println("-1");
+        }
+        
+ }
+}
